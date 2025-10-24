84b56962ccd5aaa8470741795fe26224e4618caf
SoftwareDevelopor <saurabhkumar13618@gmail.com>
2025-10-24 17:12:27 +0530

    updatefiles

--- src/controller/contact-controller.js
+++ src/controller/contact-controller.js
@@ -9,42 +9,13 @@ exports.createcontact = async (request, response) => {
     const newContact = new ContactModal(contactData);
     await newContact
       .save()
-      .then(async (result) => {
-        try {
-          const publicKey = process.env.PUBLIC_KEY;
-          const serviceID = process.env.SERVICE_ID;
-          const templateID = process.env.TEMPLATE_ID;
-          const to_email = process.env.EMAIL;
-
-          emailjs.init({
-            publicKey: publicKey,
-          });
-
-          const templateParams = {
-            name: result.name,
-            email: result.email,
-            message: result.message,
-            to_email: to_email,
-          };
-
-          emailjs.send(serviceID, templateID, templateParams).then(
-            (responseEmail) => {
-              console.log("SUCCESS!", responseEmail.status, responseEmail.text);
-            },
-            (error) => {
-              console.log("FAILED...", error);
-            }
-          );
-
-          const obj = {
-            _status: "success",
-            message: "Contact created successfully!",
-            data: result,
-          };
-          response.send(obj);
-        } catch (error) {
-          console.log(error);
-        }
+      .then((result) => {
+        const obj = {
+          _status: "success",
+          message: "Contact created successfully & Email has started sending..!",
+          data: result,
+        };
+        response.send(obj);
       })
       .catch((error) => {
         console.log(error);
